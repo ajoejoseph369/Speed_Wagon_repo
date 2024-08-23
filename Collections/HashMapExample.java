80c66569df6212d5ae563da54ba26fbfe34185dd
ajoejoseph369 <ajoe.joseph@people10.com>
2024-08-23 12:09:30 +0530

    updated1

---
+++ Collections/HashMapExample.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "Java");
+        hashMap.put(2, "Python");
+        hashMap.put(3, "C++");
+
+        System.out.println("HashMap: " + hashMap);
+    }
+}
